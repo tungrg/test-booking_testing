20c7d2031004d26bf4e9ed74ec91d709c914fc81
tungrg <18120258@student.hcmus.edu.vn>
2021-11-10 11:39:18 +0700

    delete some not needed function

--- src/main/java/source/BookingPages/bookingSearchResultPage.java
+++ src/main/java/source/BookingPages/bookingSearchResultPage.java
@@ -5,12 +5,6 @@ import org.openqa.selenium.WebElement;
 import source.core.DriverWrapper;
 
 public class bookingSearchResultPage extends baseBookingPage {
-    public String getTitle(){
-        return DriverWrapper.getDriver().getTitle();
-    }
-    public String getUrl(){
-        return DriverWrapper.getDriver().getCurrentUrl();
-    }
     public String getParisElement (){
         return DriverWrapper.getDriver().findElement(By.cssSelector("#ss")).getAttribute("value");
     }
